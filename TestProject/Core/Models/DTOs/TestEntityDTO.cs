da8de979b436eec9e05deee404b80391579bd9f1
Alexander Shefer <shefard55r@gmail.com>
2019-08-09 14:57:55 +0600

    - db context changes to add and get datetime values in utc

---
+++ TestProject/Core/Models/DTOs/TestEntityDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestProject.Core.Models.Entities;
+
+namespace TestProject.Core.Models.DTOs
+{
+    public class TestEntityDTO
+    {
+        public TestEntityDTO()
+        {
+            
+        }
+
+        public TestEntityDTO(TestEntity entity)
+        {
+            Id = entity.Id;
+            Date = entity.Date;
+            DateCreated = entity.DateCreated;
+        }
+
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
--- TestProject/Infrastructure/ApplicationDbContext.cs
+++ TestProject/Infrastructure/ApplicationDbContext.cs
@@ -18,31 +18,31 @@ namespace TestProject.Infrastructure
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            //// Make DateTime values' kind be UTC by default
-            //foreach (var entityType in builder.Model.GetEntityTypes())
-            //{
-            //    foreach (var prop in entityType.GetProperties())
-            //    {
-            //        if (prop.ClrType == typeof(DateTime))
-            //        {
-            //            builder
-            //                .Entity(entityType.ClrType)
-            //                .Property<DateTime>(prop.Name)
-            //                .HasConversion(
-            //                    x => x.ToUniversalTime(),
-            //                    x => DateTime.SpecifyKind(x, DateTimeKind.Utc));
-            //        }
-            //        else if (prop.ClrType == typeof(DateTime?))
-            //        {
-            //            builder
-            //                .Entity(entityType.ClrType)
-            //                .Property<DateTime?>(prop.Name)
-            //                .HasConversion(
-            //                    x => x.HasValue ? x.Value.ToUniversalTime() : x,
-            //                    x => x.HasValue ? DateTime.SpecifyKind(x.Value, DateTimeKind.Utc) : x);
-            //        }
-            //    }
-            //}
+            // Make DateTime values' kind be UTC by default
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                foreach (var prop in entityType.GetProperties())
+                {
+                    if (prop.ClrType == typeof(DateTime))
+                    {
+                        builder
+                            .Entity(entityType.ClrType)
+                            .Property<DateTime>(prop.Name)
+                            .HasConversion(
+                                x => x.ToUniversalTime(),
+                                x => DateTime.SpecifyKind(x, DateTimeKind.Utc));
+                    }
+                    else if (prop.ClrType == typeof(DateTime?))
+                    {
+                        builder
+                            .Entity(entityType.ClrType)
+                            .Property<DateTime?>(prop.Name)
+                            .HasConversion(
+                                x => x.HasValue ? x.Value.ToUniversalTime() : x,
+                                x => x.HasValue ? DateTime.SpecifyKind(x.Value, DateTimeKind.Utc) : x);
+                    }
+                }
+            }
         }
     }
 }
